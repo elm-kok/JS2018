c2cb6dd13581d27f3cac6787efdf8ac2b21c5ccf
elm-kok <polindays@gmail.com>
2017-12-30 19:14:56 +0700

    add update Flash

--- app.js
+++ app.js
@@ -3,12 +3,45 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
 mongoose.connect('mongodb://localhost/myapp');
 let db = mongoose.connection;
 db.once('open', () => {
@@ -60,6 +93,7 @@ app.post('/product/add', (req, res) => {
             throw err;
             return;
         }
+        req.flash('success', 'Product Added');
         res.redirect('/');
     });
 });
@@ -75,6 +109,7 @@ app.post('/product/edit/:id', (req, res) => {
             throw err;
             return;
         }
+        req.flash('success','Product updated');
         res.redirect('/');
     });
 });
